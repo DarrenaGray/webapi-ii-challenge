4dc61b36a0f7d0f4774461384207fb9c087a3856
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-23 18:47:12 -0400

    Installed cors and imported to server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const postsRouter = require('./data/db-router');
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
