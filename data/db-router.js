c5a711af210cd82ec2493914411299c858e20cd7
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-23 17:33:58 -0400

    Created endpoints for GET request by ID and POST request. Need to solve issue with Error statuses not showing

--- data/db-router.js
+++ data/db-router.js
@@ -16,4 +16,41 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+    if (req.body.title || req.body.contents) {
+        try {
+            const post = await db.insert(req.body);
+            res.status(201).json(post);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: "There was an error while saving the post to the database"
+            })
+        }
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post"
+        });
+    }
+});
+
 module.exports = router;
